d88f8a55d674e04f6228d223600d6eca630011dc
ensarcehajic <ensar.cehajic@fet.ba>
2025-04-19 19:17:15 +0200

    Fixed models/database.py

--- App/models/database.py
+++ App/models/database.py
@@ -2,10 +2,10 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class Korisnik(db.Model):
-    __tablename__ = 'korisnici'
+class User(db.Model):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     username = db.Column(db.String(50), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)
-    uloga = db.Column(db.String(20), nullable=False)
+    role = db.Column(db.String(20), nullable=False)
